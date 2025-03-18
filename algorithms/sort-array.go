e11549db6e06da4fa714c77d6579d1ec6908382e
Cerecero <ale.rey.cer@outlook.com>
2025-03-18 16:37:19 -0600

    added linearsearch example

--- algorithms/sort-array.go
+++ algorithms/sort-array.go
@@ -3,16 +3,39 @@ import "fmt"
 
 func insertInOrder(slice []int, val int) []int{
 	i := 0
+	//Find the right index to where to insert the value
+	// If i is less than the length of the slice
+	// And the value in slice[i] is less than the given value
+	// Then increment i to find the right index
 	for i < len(slice) && slice[i] < val {
 		i++
 	}
-
+	
+	// Grow the slice, (append inserts at the end of the slice)
 	slice = append(slice,0)
-
+	
+	// copy(dst, src)
+	// This will copy from a src (slice[i:]) into a dst (slice[i+1:])
+	// dst slice starts from the index + 1 to the end of the slice or len(slice)
+	// src starts from index to the end of the slice
 	copy(slice[i+1:], slice[i:])
 
+	// insert into i the value
 	slice[i] = val
 	return slice
+}
+
+func linearSearch(arr []int, val int) (int, bool){
+	for i, v := range arr {
+		if val == v {
+			return i, true
+		}
+
+		if v > val{
+			break
+		}
+	}
+	return 0, false
 }
 
 func main(){
@@ -24,4 +47,11 @@ func main(){
 	fmt.Println("After inserting 25: ", slice)
 	slice = insertInOrder(slice, -23)
 	fmt.Println("After inserting -23: ", slice)
+
+
+	search, found := linearSearch(slice, 32)
+	fmt.Printf("Searching for 32: Found at index %d, %t\n", search, found)
+
+	search, found = linearSearch(slice, 19)
+	fmt.Printf("Searching for 19: Not found %t", found)
 }
